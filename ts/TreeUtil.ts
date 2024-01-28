c8e7e591278d654396d295c2894d6b81d82f0c02
Scott Anderson <scottanderson@users.noreply.github.com>
2024-01-28 17:25:32 -0500

    Industries (#120)
    * Add files via upload
    
    ---------
    
    Co-authored-by: illudedPerception <126168696+illudedPerception@users.noreply.github.com>

--- ts/TreeUtil.ts
+++ ts/TreeUtil.ts
@@ -209,6 +209,18 @@ function industryFilter(industry: Industry, tree: Vector): boolean {
             return radiusFilter(industry.location, tree, 35_00); // 35m
         case 'firewooddepot':
             return radiusFilter(industry.location, tree, 15_00); // 15m
+        case 'WaterWell':
+            return radiusFilter(industry.location, tree, 10_00); // 10m
+        case 'Sandhouse':
+            return radiusFilter(industry.location, tree, 10_00); // 10m
+        case 'WheatFarm':
+            return radiusFilter(industry.location, tree, 42_00); // 42m
+        case 'MeatPackingPlant':
+            return radiusFilter(industry.location, tree, 37_00); // 37m
+        case 'CattleFarm':
+            return radiusFilter(industry.location, tree, 46_00); // 46m
+        case 'Woodrick':
+            return radiusFilter(industry.location, tree, 5_00); // 5m
         case 'enginehouse_alpine':
         case 'enginehouse_aspen':
         case 'enginehouse_barn':
